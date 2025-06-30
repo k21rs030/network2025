dd4a5719557b8695ca3bdd245ed5323146b46079
k21rs030 <113829983+k21rs030@users.noreply.github.com>
2025-06-30 12:35:27 +0900

    Update httpserver1.rb

--- src/httpserver1.rb
+++ src/httpserver1.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/ruby
 require "socket"
-
+# ああああ
 def server client
   fl = true
   loop do
@@ -9,15 +9,38 @@ def server client
     pp b
     if fl == true
       path = b.split(" ")[1]
-      client.puts "HTTP/1.0 200 OK"
-      client.puts
-      if path == "/hello"
+      case path
+      when "/hello"
+        client.puts "HTTP/1.0 200 OK"
+        client.puts
         client.puts("hello world")
-      else
-        client.puts(path)
-      end
-      
+      when "/93"
+        client.puts "HTTP/1.1 301 Moved Permanently"
+        client.puts "Location:  http://www.kyusan-u.ac.jp/"
 
+        client.puts()
+      else
+        client.puts "HTTP/1.0 200 OK"
+        client.puts("Content-Type: text/plain; charset=UTF-8")
+        client.puts
+        
+        begin
+
+          File.open("."+path,"r") do |file|
+            while line = file.gets&.chomp
+                client.puts (line)
+            end
+          end
+          client.puts(path)
+                  
+        rescue 
+          client.puts("File not found or cannot be opened.")
+        end
+        
+
+      
+      
+      end
       fl=false
     end
 
